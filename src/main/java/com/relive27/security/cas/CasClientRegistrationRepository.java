1c0397c7d3ea4bcecf94a4f50f1b1c1258ed5730
ReLive27 <re_live27@163.com>
2023-05-25 22:21:18 +0800

    Add CasClientRegistrationRepository

---
+++ src/main/java/com/relive27/security/cas/CasClientRegistrationRepository.java
@@ -0,0 +1,10 @@
+package com.relive27.security.cas;
+
+/**
+ * @author: ReLive27
+ * @date: 2023/5/25 19:58
+ */
+public interface CasClientRegistrationRepository {
+
+    CasClientRegistration loadCasClientRegistration();
+}
